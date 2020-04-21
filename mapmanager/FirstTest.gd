0f2ac8e9eb1013deb1bc22c94783acc9a778a324
HaSa1002 <johawitt@outlook.de>
2020-04-21 15:02:58 +0200

    Improve Shader Highlighting

--- mapmanager/FirstTest.gd
+++ mapmanager/FirstTest.gd
@@ -5,7 +5,8 @@ static func vec4(a: Vector2, b: Vector2) -> Color:
 	return Color(a.x,a.y,b.x,b.y)
 
 func _process(delta):
-	material.set_shader_param("mouse", get_global_mouse_position()/rect_size)
+	rect_size.y = rect_size.x
+	material.set_shader_param("mouse", get_local_mouse_position()/rect_size)
 	material.set_shader_param("delta", delta)
 	#printt(get_viewport().size, get_viewport().get_mouse_position())
 	material.set_shader_param("rects", floor(get_viewport().size.x / 100.0))
